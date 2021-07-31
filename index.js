d3c2f7f8c2ae80aa5c6b6f666ea4d4879206b898
Abraham Jose Gonzalez Barboza <jgab.net@gmail.com>
2021-07-31 16:33:49 -0400

    chore: change package names

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import sayHello from '@is-pawa/package-b';
-import sayBye from '@is-pawa/package-a';
+import sayHello from 'is-pawa-package-b';
+import sayBye from 'is-pawa-package-a';
 
 sayHello("Oscar");
 sayBye("Oscar");
